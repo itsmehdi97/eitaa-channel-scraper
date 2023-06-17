5545378a5ecf4df4bbae5462df6ef3cc69d9ef65
mehdi <itemehdi97>
2023-06-17 13:07:49 +0330

    use schema for channel schedule objs

--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,21 @@
+from typing import Optional
+from datetime import datetime
+
 from pydantic import BaseModel
+
+from core.config import get_settings
+
+
+SETTINGS = get_settings()
 
 
 class ChannelSchedule(BaseModel):
     channel_name: str
+    channel_id: Optional[str]
+    refresh_interval: Optional[int] = SETTINGS.CHANNEL_REFRESH_INTERVAL
+    offset: Optional[int] = 1
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+
+    info: Optional[str]  # TODO: remove this field
+
